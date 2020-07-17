7a7e408c40a8f91b87f95aaf86fd418a2147e40d
neneta0921 <info@neneta.net>
2020-07-18 08:32:42 +0900

    php artisan ui vue --auth

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 // APIのURL以外のリクエストに対してはindexテンプレートを返す
 // 画面遷移はフロントエンドのVueRouterが制御する
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .version();
-
-mix.browserSync({
-    files: [
-      "resources/views/**/*.blade.php",
-      "public/**/*.*"
-    ],
-    proxy: 'http://127.0.0.1:8000'
-});
+    .sass('resources/sass/app.scss', 'public/css');
