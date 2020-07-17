1b92bf4d927a4f84a3b799f1591c3ec591d6d1f0
neneta0921 <info@neneta.net>
2020-07-18 06:53:50 +0900

    ルート定義

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
