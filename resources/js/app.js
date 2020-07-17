433c353e371a1ee142b5e0f3ae4a13d73ccfb86d
neneta0921 <info@neneta.net>
2020-07-17 21:34:04 +0900

    ch-3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>Hello world</h1>'
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
 })
